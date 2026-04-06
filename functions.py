fc0d6838f078411713e2ccc2263770c046687c88
Julian Aileru <jaileru@unc.edu>
2026-04-06 10:01:57 -0400

    04.06.2026

--- functions.py
+++ functions.py
@@ -15,49 +15,16 @@ from rpy2.robjects.conversion import localconverter
 ropls = importr('ropls')
 base = importr('base')
 
-def OPLSDA(data,metadata,y_var='timepoint_r',applylog=True):
-    X = data.copy()
-    X = X.loc[[i for i in X.index if i in metadata.index], :]
-    if applylog:
-        X = np.log2(X+1)
-    # Y: response vector aligned to X
-    Y = metadata.loc[X.index, y_var]  # adjust column name as needed
-    
-    # --- Convert to R objects ---
-    with localconverter(ro.default_converter + pandas2ri.converter):
-        X_r = ro.conversion.py2rpy(X)          # R matrix (samples x features)
-        Y_r = ro.StrVector(Y.tolist())          # character vector for PLS-DA
-    # --- Run OPLS-DA (1 predictive + orthogonal components) ---
-    oplsda_model = ropls.opls(
-        X_r,
-        Y_r,
-        predI  = 1,
-        orthoI = ro.NA_Integer,   # auto-select number of orthogonal components
-    )
-    
-    
-    with localconverter(ro.default_converter + pandas2ri.converter):
-        # Scores (T scores = predictive, To = orthogonal)
-        T_scores  = ro.conversion.rpy2py(ropls.getScoreMN(oplsda_model))
-        To_scores = ro.conversion.rpy2py(ropls.getScoreMN(oplsda_model, orthoL=True))
-    
-        # Loadings
-        P_load  = ro.conversion.rpy2py(ropls.getLoadingMN(oplsda_model))
-        Po_load = ro.conversion.rpy2py(ropls.getLoadingMN(oplsda_model, orthoL=True))
-
-        # VIP scores (predictive importance)
-        vip = ro.conversion.rpy2py(ropls.getVipVn(oplsda_model))
-        model_df = ro.conversion.rpy2py(oplsda_model.slots["modelDF"])
-    T_df  = pd.DataFrame(T_scores,  index=X.index)
-    To_df = pd.DataFrame(To_scores, index=X.index)
-    vip_s = pd.Series(vip, index=X.columns, name='VIP')
-    display = pd.concat([T_df, To_df.iloc[:, 0]], axis=1)
-    display.columns=['Predictive',"Orthogonal"]
-    display['sample_type'] = metadata.loc[display.index, y_var]
-    sns.scatterplot(display,x='Predictive',y='Orthogonal',hue='sample_type')
-    plt.title("OPLS-DA\n Predictive Component")
-    return {"Predictive":T_df,"Orthogonal":To_df,"Predictive_Loadings":P_load,"Orthogonal_Loadings":Po_load,"VIP_Scores":vip_s,"Model_Statistics":model_df}
-            
+def TIC(data, scale=True):
+    n_samples, n_signals = data.shape
+    print(f"Signals:{n_signals}\nSamples:{n_samples}")
+    scaler = data.sum(axis=1).mean()
+    tic = data.div(data.sum(axis=1), axis=0)
+    if not np.isclose(tic.sum(axis=1), 1.0).all():
+        raise ValueError("Total ion intensity should sum to 1")
+    if scale:
+        tic = tic * scaler
+    return tic
     
 
 def CV(data, log2=False, threshold=30, qc="_NIST_", filter_by='nonparametric', apply_filter=False):
